23f234c02fb23944bc9cfd6f5946e37ef9bcc2db
satrirattt <s6606021620198@email.kmutnb.ac.th>
2025-03-10 22:19:10 +0700

    aa

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const express = require('express');
-const { Sequelize, sequelize, Product, Order, Payment, Customer ,MaterialProduct,Material,Delivery,Employees,customerId,Promotion} = require('node76886-nodeproject.proen.app.ruk-com.cloud:11840/backendProjs-main/index.js');
+const { Sequelize, sequelize, Product, Order, Payment, Customer ,MaterialProduct,Material,Delivery,Employees,customerId,Promotion} = require('11840/backendProjs-main/index.js');
 const app = express();
 app.use(express.json());
 const port = 3000;
